6cfc1457fbc25945cfbc79a4c7a63d85ed42aa37
alvinkenyagah <alvin47k@gmail.com>
2023-03-14 08:47:26 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code in this file!
+
+const currentUser = 'Alvin Kenyagah';
+
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+let excitedWelcomeMessage = welcomeMessage.toUpperCase() +"!" ;
+
+let shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
+console.log(shortGreeting);
+
+
